8461fb6d1f58407835df453d2b25503990cb7a2c
zhongyi <yi.zhong@xgo.one>
2023-11-17 16:24:37 +0800

    docker

--- config/config.test.ts
+++ config/config.test.ts
@@ -11,5 +11,11 @@ export default {
 	// jwt sign secret
 	jwt: {
 		secret: process.env.JWT_SECRET
+	},
+	redis: {
+		host: process.env.REDIS_HOST, // default value
+		port: parseInt(process.env.REDIS_PORT, 10), // default value
+		password: process.env.REDIS_PASSWORD,
+		db: process.env.REDIS_DB
 	}
 }

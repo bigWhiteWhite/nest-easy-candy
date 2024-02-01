3fcf4e1d7198c4b289ffeed6de436ce59af0cfb3
zhongyi <yi.zhong@xgo.one>
2024-02-01 17:07:06 +0800

    md

--- scripts/mongodb_rs_init.sh
+++ scripts/mongodb_rs_init.sh
@@ -4,20 +4,20 @@ m1=mongod1
 m2=mongod2
 m3=mongod3
 port=${PORT:-27017}
+rootUser="$MONGO_LOGIN"
+rootPassword="$MONGO_PASSWORD"
 
-echo "###### Waiting for ${m1} instance startup.."
+echo "###### 等待实例 ${m1} 启动.."
 until mongosh --host ${m1}:${port} --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' &>/dev/null; do
   printf '.'
   sleep 1
 done
-echo "###### Working ${m1} instance found, initiating user setup & initializing rs setup.."
+echo "###### 发现工作 ${m1} 实例, 初始化用户设置 & 初始化rs设置..."
 
 # setup user + pass and initialize replica sets
+# var admin = db.getSiblingDB('admin');
 mongosh --host ${m1}:${port} <<EOF
-var rootUser = '$MONGO_LOGIN';
-var rootPassword = '$MONGO_PASSWORD';
-var admin = db.getSiblingDB('admin');
-admin.auth(rootUser, rootPassword);
+db.auth('$rootUser', '$rootPassword');
 
 var config = {
     "_id": "${MONGO_REPLICA_NAME}",
@@ -26,16 +26,16 @@ var config = {
         {
             "_id": 1,
             "host": "${m1}:${port}",
-            "priority": 2
+            "priority": 3
         },
         {
             "_id": 2,
-            "host": "${m2}:${port}",
-            "priority": 1
+            "host": "${m2}:27018",
+            "priority": 2
         },
         {
             "_id": 3,
-            "host": "${m3}:${port}",
+            "host": "${m3}:27019",
             "priority": 1,
             "arbiterOnly": true
         }
